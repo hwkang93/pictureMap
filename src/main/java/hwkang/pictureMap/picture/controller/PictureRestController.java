b43a7be8bb194aaac5a8c105ea17f6eab95f3bb6
hwkang93 <landman93@naver.com>
2021-11-19 17:09:57 +0900

    thymeleaf 로 화면 만드는 중

--- src/main/java/hwkang/pictureMap/picture/controller/PictureRestController.java
+++ src/main/java/hwkang/pictureMap/picture/controller/PictureRestController.java
@@ -1,8 +1,19 @@
 package hwkang.pictureMap.picture.controller;
 
+import hwkang.pictureMap.picture.dto.Picture;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class PictureRestController {
+
+    @GetMapping("/pictures")
+    public List<Picture> findAll() {
+
+        return new ArrayList<>();
+    }
 
 }
