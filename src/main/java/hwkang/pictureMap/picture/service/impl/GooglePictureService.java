43f8a3ccfedcef11b17c5ae639cf27c3bbd47cd5
hwkang93 <landman93@naver.com>
2021-11-19 14:55:25 +0900

    Google Drive API 연동중

--- src/main/java/hwkang/pictureMap/picture/service/impl/GooglePictureService.java
+++ src/main/java/hwkang/pictureMap/picture/service/impl/GooglePictureService.java
@@ -1,24 +1,9 @@
 package hwkang.pictureMap.picture.service.impl;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.drive.DriveScopes;
 import hwkang.pictureMap.picture.dto.Picture;
 import hwkang.pictureMap.picture.service.PictureService;
 import org.springframework.stereotype.Service;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Collections;
 import java.util.List;
 
 /*
