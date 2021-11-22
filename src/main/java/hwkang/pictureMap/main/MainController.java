7d64b283419d1d1c06b94f7b9790db47a7810d82
hwkang93 <landman93@naver.com>
2021-11-22 15:44:12 +0900

    MainController 추가

---
+++ src/main/java/hwkang/pictureMap/main/MainController.java
@@ -0,0 +1,14 @@
+package hwkang.pictureMap.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/main")
+    public String mainView() {
+
+        return "main";
+    }
+}
